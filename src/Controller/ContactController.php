6a9886e317884f88995659cc679437dd5946aa13
timotheJeannot <timothe.jeannot@gmail.com>
2019-05-10 17:53:48 +0200

    10/05/2019

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -12,8 +12,16 @@ class ContactController extends AbstractController
      */
     public function index()
     {
+
+       //https://codereviewvideos.com/course/symfony-4-beginners-tutorial/video/symfony-4-contact-form
+       //https://codereviewvideos.com/course/symfony-4-beginners-tutorial/video/send-email-symfony-4
+       //https://symfony.com/doc/current/email.html 
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
     }
+
+    // ici  il va falloir gérer une autre route avec l'id d'un événement ou d'un article
+    // avec un autre type de formulaire 
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,13 +5,30 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        //https://symfony.com/doc/current/reference/forms/types/choice.html
         $builder
-            ->add('field_name')
+            ->add('sujet' , ChoiceType::class ,[
+                'choices'=> [
+                    'Campus des métiers et des qualifications'=> 'Campus des métiers et des qualifications',
+                    'Les plateformes technologiques' => 'Les plateformes technologiques',
+                    'Les comités locaux école-entreprise' => 'Les comités locaux école-entreprise',
+                    'Les référents école-entreprise en collège' => 'Les référents école-entreprise en collège',
+                    'Les conventions'=> 'Les conventions',
+                    'Le lycée des métiers' => 'Le lycée des métiers',
+                    'Les conseillers entreprise pour l\'école ' => 'Les conseillers entreprise pour l\'école',
+                ]
+                
+            ])
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('contenu')
         ;
     }
 
