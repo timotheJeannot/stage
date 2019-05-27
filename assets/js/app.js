d0c9a58cd812f6df893c3506aeb28e408ab32810
timotheJeannot <timothe.jeannot@gmail.com>
2019-05-27 15:21:59 +0200

    27/05/2019

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,9 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+//require('../css/app.css');
 //require('../css/blog-home.css');
-//require('../css/bootstrap.min.css');
+require('../css/bootstrapTJ.min.css');
 
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
